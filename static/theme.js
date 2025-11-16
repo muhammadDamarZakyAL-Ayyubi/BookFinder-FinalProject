537d7c164481ea2adcd21203ae53fde5d28038f7
muhammadDamarZakyAL-Ayyubi <muhammad3328ft.2023@student.uny.ac.id>
2025-11-17 00:46:46 +0700

    Delete static/theme.js

--- static/theme.js
+++
@@ -1,9 +0,0 @@
-const btn = document.getElementById("toggle-theme");
-const body = document.body;
-
-btn.addEventListener("click", () => {
-    body.classList.toggle("light");
-    body.classList.toggle("dark");
-
-    btn.textContent = body.classList.contains("light") ? "☀️" : "🌙";
-});
